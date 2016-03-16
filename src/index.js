f9b8c557221834b2d75234d98c88c4bc4136c60d
Plusb Preco <plusb21@gmail.com>
2016-03-16 12:06:11 +0900

    Cleanup codes
    * Add error handling

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 const defaultMessage = 'Are you sure?';
 
-let listener = null;
+let listener;
 
 function preventUnload(message = defaultMessage) {
-  listener = (event) => {
-    // for cross-browser solution, see: https://developer.mozilla.org/en-US/docs/Web/Events/beforeunload
-    event.returnValue = message;
-    return message;
-  };
+  if (listener) {
+    return;
+  }
 
+  listener = makeHandler(message);
   window.addEventListener('beforeunload', listener);
 }
 
 function revoke() {
-  if (listener) {
-    window.removeEventListener('beforeunload', listener);
-    listener = null;
+  if (!listener) {
+    return;
   }
+
+  window.removeEventListener('beforeunload', listener);
+  listener = null;
+}
+
+function makeHandler(message) {
+  return () => {
+    // for cross-browser solution, see: https://developer.mozilla.org/en-US/docs/Web/Events/beforeunload
+    event.returnValue = message;
+    return message;
+  };
 }
 
 export {
