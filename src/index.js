de09ae8ff4e254d34bdd59a3fdd2dc4e14e1389e
Plusb Preco <plusb21@gmail.com>
2017-04-02 00:52:12 +0900

    Polish things

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 function preventUnload(message) {
-  const handler = makeHandler(message);
-
+  const handler = createHandler(message);
   window.addEventListener('beforeunload', handler);
 
   return handler;
@@ -14,11 +13,12 @@ function cancel(handler) {
   window.removeEventListener('beforeunload', handler);
 }
 
-function makeHandler(message = '') {
+function createHandler(message = '') {
   return (event) => {
     // Cross-browser solution
     // https://developer.mozilla.org/en-US/docs/Web/Events/beforeunload
-    event.returnValue = message; // eslint-disable-line no-param-reassign
+    // eslint-disable-next-line no-param-reassign
+    event.returnValue = message;
 
     return message;
   };
